14916fb730cf2e2a253b15334d722b05fb211512
Jane <eg@4xxi.com>
2015-10-27 13:25:54 +0300

    redux-form implemented for registration form

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {reduxForm} from 'redux-form';
 
 class Registration extends React.Component {
+  static propTypes = {
+    fields: PropTypes.object.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+  };
   render() {
+    const {
+      fields: { email, password, confirmPassword },
+      handleSubmit,
+    } = this.props;
+    const renderInput = (field, placeholder, icon) =>
+      <div className={'input-wrap input-wrap_with-icon' + (field.error && field.touched ? ' input-wrap_error' : '')}>
+          <div className="input-wrap__icon"><span aria-hidden="true" className={'glyphicon ' + icon }></span></div>
+          <input type="text" className="text full-width" placeholder={placeholder} />
+          {
+            field.error && field.touched ?
+              <div className="input-wrap__error-msg">
+                <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                 {field.error}
+              </div>
+              : null
+          }
+      </div>;
     return (
       <div className="container container-1">
           <div className="login-block">
               <img src={require('../../public/images/logo-big.png')} alt=""/>
               <div className="login-block__site-title">Worth.fm</div>
               <div className="login-block__site-descr">Invest in possibility.</div>
-              <form action="" className="common-form login-form">
-                  <div className="input-wrap input-wrap_with-icon">
-                      <div className="input-wrap__icon"><span className="glyphicon glyphicon-user" aria-hidden="true"></span></div>
-                      <input type="text" className="text full-width" placeholder="example@website.com" />
-                  </div>
-                  <div className="input-wrap input-wrap_with-icon">
-                      <div className="input-wrap__icon"><span className="glyphicon glyphicon-lock" aria-hidden="true"></span></div>
-                      <input type="pasword" className="text full-width" placeholder="Password" />
-                  </div>
-                  <div className="input-wrap input-wrap_with-icon">
-                      <div className="input-wrap__icon"><span className="glyphicon glyphicon-lock" aria-hidden="true"></span></div>
-                      <input type="pasword" className="text full-width" placeholder="Confirm Password" />
-                  </div>
+              <form onSubmit={handleSubmit} className="common-form login-form">
+                  {renderInput(email, 'Email', 'glyphicon-user')}
+                  {renderInput(password, 'Password', 'glyphicon-lock')}
+                  {renderInput(confirmPassword, 'Confirm password', 'glyphicon-lock')}
+
                   <div className="input-wrap">
                       <button className="btn btn_blue w-308">Sign Up</button>
                   </div>
@@ -31,5 +45,10 @@ class Registration extends React.Component {
     );
   }
 }
+
+Registration = reduxForm({
+  form: 'registration',
+  fields: ['email', 'password', 'confirmPassword'],
+})(Registration);
 
 export default Registration;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router} from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
+import {createStore, combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';
 
 import routes from './routes';
+
+const reducers = {
+  registrationForm: formReducer
+};
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
 
 let history = createBrowserHistory();
 
