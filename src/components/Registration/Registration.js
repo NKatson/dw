75b1ef2c9624042dc6e97a8ce4ceeacd96c54df2
Jane <eg@4xxi.com>
2015-10-27 12:36:04 +0300

    Registration page added

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Registration extends React.Component {
   render() {
-    return <div>Registration</div>;
+    return (
+      <div className="container container-1">
+          <div className="login-block">
+              <img src={require('../../public/images/logo-big.png')} alt=""/>
+              <div className="login-block__site-title">Worth.fm</div>
+              <div className="login-block__site-descr">Invest in possibility.</div>
+              <form action="" className="common-form login-form">
+                  <div className="input-wrap input-wrap_with-icon">
+                      <div className="input-wrap__icon"><span className="glyphicon glyphicon-user" aria-hidden="true"></span></div>
+                      <input type="text" className="text full-width" placeholder="example@website.com" />
+                  </div>
+                  <div className="input-wrap input-wrap_with-icon">
+                      <div className="input-wrap__icon"><span className="glyphicon glyphicon-lock" aria-hidden="true"></span></div>
+                      <input type="pasword" className="text full-width" placeholder="Password" />
+                  </div>
+                  <div className="input-wrap input-wrap_with-icon">
+                      <div className="input-wrap__icon"><span className="glyphicon glyphicon-lock" aria-hidden="true"></span></div>
+                      <input type="pasword" className="text full-width" placeholder="Confirm Password" />
+                  </div>
+                  <div className="input-wrap">
+                      <button className="btn btn_blue w-308">Sign Up</button>
+                  </div>
+                  <div>Already have an account? <a href="#">Sign In.</a></div>
+              </form>
+          </div>
+      </div>
+    );
   }
 }
 
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
-import {Link} from 'react-router';
 
+import {Registration} from '../../components';
 require('./styles');
 
 class App extends React.Component {
@@ -9,11 +9,24 @@ class App extends React.Component {
   };
   render() {
     return (
-      <div>
-        <Link to="/reset">Reset</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
-          {this.props.children}
+      <div className="main-wrap">
+        <div className="wide-block main-header">
+            <div className="container container-1">
+                <a href="#" className="main-logo"></a> Text
+                <div className="cabinet">
+                    <span></span>
+                </div>
+            </div>
+        </div>
+        <div className="wide-block">
+          <Registration />
+        </div>
+        <div className="wide-block main-footer">
+            <div className="container container-1">
+                <div className="main-footer__copy"><span></span></div>
+                Disclaimer
+            </div>
+        </div>
       </div>
     );
   }
