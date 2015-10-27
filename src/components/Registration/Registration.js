6b427fc659ec7f51c738017c27cbceb75d238c69
Jane <eg@4xxi.com>
2015-10-27 15:14:00 +0300

    WIP

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,30 +1,11 @@
 import React, {PropTypes} from 'react';
 import {connectReduxForm} from 'redux-form';
-
-function validateRegistration(data) {
-  const errors = {};
-  if (!data.email) {
-    errors.email = 'Required';
-  }
-  const emailReg = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-  if (data.email && !emailReg.test(data.email)) {
-    errors.email = 'Please use valid email address';
-  }
-  if (data.password && data.password.length < 8) {
-    errors.password = 'Passwords must be minimum 8 characters long';
-  }
-  if (!data.confirmPassword) {
-    errors.confirmPassword = 'Please enter password again';
-  } else if (data.password !== data.confirmPassword) {
-    errors.confirmPassword = 'Passwords are not equal';
-  }
-  return errors;
-}
+import validation from './validation';
 
 @connectReduxForm({
   form: 'form',
   fields: ['email', 'password', 'confirmPassword'],
-  validate: validateRegistration,
+  validate: validation,
 })
 export default class Registration extends React.Component {
   static propTypes = {
@@ -36,10 +17,10 @@ export default class Registration extends React.Component {
       fields: { email, password, confirmPassword },
       handleSubmit,
     } = this.props;
-    const renderInput = (field, placeholder, icon) =>
+    const renderInput = ({ field, placeholder, icon, type = 'text' }) =>
       <div className={'input-wrap input-wrap_with-icon' + (field.error && field.touched ? ' input-wrap_error' : '')}>
           <div className="input-wrap__icon"><span aria-hidden="true" className={'glyphicon ' + icon }></span></div>
-          <input type="text" className="text full-width" placeholder={placeholder} {...field} />
+          <input type={type} className="text full-width" placeholder={placeholder} {...field} />
           {
             field.error && field.touched ?
               <div className="input-wrap__error-msg">
@@ -56,10 +37,24 @@ export default class Registration extends React.Component {
               <div className="login-block__site-title">Worth.fm</div>
               <div className="login-block__site-descr">Invest in possibility.</div>
               <form onSubmit={handleSubmit} className="common-form login-form">
-                  {renderInput(email, 'Email', 'glyphicon-user')}
-                  {renderInput(password, 'Password', 'glyphicon-lock')}
-                  {renderInput(confirmPassword, 'Confirm password', 'glyphicon-lock')}
-
+                  {renderInput({
+                    field: email,
+                    placeholder: 'Email',
+                    icon: 'glyphicon-user',
+                    type: 'email',
+                  })}
+                  {renderInput({
+                    field: password,
+                    placeholder: 'Password',
+                    icon: 'glyphicon-lock',
+                    type: 'password',
+                  })}
+                  {renderInput({
+                    field: confirmPassword,
+                    placeholder: 'Confirm password',
+                    icon: 'glyphicon-user',
+                    type: 'password',
+                  })}
                   <div className="input-wrap">
                       <button onClick={handleSubmit} className="btn btn_blue w-308">Sign Up</button>
                   </div>
---
+++ src/components/Registration/validation.js
@@ -0,0 +1,27 @@
+export default function validation(data) {
+  const errors = {};
+  if (!data.email) {
+    errors.email = 'Required';
+  }
+  const emailReg = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  if (data.email && !emailReg.test(data.email)) {
+    errors.email = 'Please use valid email address';
+  }
+  if (data.password && data.password.length < 8) {
+    errors.password = 'Passwords must be minimum 8 characters long';
+  }
+  if (!data.confirmPassword) {
+    errors.confirmPassword = 'Please enter password again';
+  }
+  if (data.password && !errors.password) {
+    if (!/^(?=.*[A-Z]).*$/.test(data.password)) {
+      errors.password = 'Password must contain at least one uppercase letter (A-Z)';
+    } else if (!/^(?=.*[0-9]).*$/.test(data.password)) {
+      errors.password = 'Password must contain at least one number (0-9)';
+    }
+  }
+  if (data.password && data.confirmPassword && data.confirmPassword !== data.password) {
+    errors.confirmPassword = 'Passwords must be equal';
+  }
+  return errors;
+}
