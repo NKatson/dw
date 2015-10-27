65594de6125a73474900fdb4d073eb7ba8b307d6
Jane <eg@4xxi.com>
2015-10-27 17:26:05 +0300

    Remove empty styles

--- src/containers/App/styles.js
+++ src/containers/App/styles.js
@@ -1,4 +1,3 @@
 require('../../public/styles/ball-spin-clockwise.min.css');
 require('../../public/styles/bootstrap.min.css');
-require('../../public/styles/elements.css');
 require('../../public/styles/styles.css');
