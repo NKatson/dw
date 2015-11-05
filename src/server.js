316cd12305de9312f67c34df0c281ee6ca30fce0
Jane <eg@4xxi.com>
2015-11-05 13:37:08 +0300

    WIP

--- src/server.js
+++ src/server.js
@@ -1,37 +1,94 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config.dev');
+import path from 'path';
+import express from 'express';
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+import favicon from 'serve-favicon';
+import createStore from './redux/create';
+import ApiClient from './helpers/ApiClient';
+import Html from './helpers/Html';
+import PrettyError from 'pretty-error';
 
-var app = express();
-var compiler = webpack(config);
+import {ReduxRouter} from 'redux-router';
+import createHistory from 'history/lib/createMemoryHistory';
+import {reduxReactRouter, match} from 'redux-router/server';
+import {Provider} from 'react-redux';
+import qs from 'query-string';
+import getRoutes from './routes';
+import getStatusFromRoutes from './helpers/getStatusFromRoutes';
 
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  contentBase: 'http://localhost:3001',
-  noInfo: true,
-  quiet: true,
-  hot: true,
-  publicPath: config.output.publicPath,
-  inline: true,
-  lazy: false,
-  headers: { "Access-Control-Allow-Origin": "*" },
-  stats: {colors: true},
-});
+const pretty = new PrettyError();
+const app = express();
 
-var hotMiddleware = require('webpack-hot-middleware')(compiler);
+const config = {
+  port: 3000,
+  host: 'localhost',
+};
 
-app.use(devMiddleware);
-app.use(hotMiddleware);
+app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(require('serve-static')(path.join(__dirname, '..', 'static')));
 
-app.listen(3000, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-    return;
+app.use((req, res) => {
+  if (__DEVELOPMENT__) {
+    // Do not cache webpack stats: the script file would change since
+    // hot module replacement is enabled in the development env
+    webpackIsomorphicTools.refresh();
+  }
+  const client = new ApiClient(req);
+
+  const store = createStore(reduxReactRouter, getRoutes, createHistory, client);
+
+  function hydrateOnClient() {
+    res.send('<!doctype html>\n' +
+      ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} store={store}/>));
   }
 
-  console.log('Listening at http://localhost:3000');
+  if (__DISABLE_SSR__) {
+    hydrateOnClient();
+    return;
+  }
+  store.dispatch(match(req.originalUrl, (error, redirectLocation, routerState) => {
+    if (redirectLocation) {
+      res.redirect(redirectLocation.pathname + redirectLocation.search);
+    } else if (error) {
+      console.error('ROUTER ERROR:', pretty.render(error));
+      res.status(500);
+      hydrateOnClient();
+    } else if (!routerState) {
+      res.status(500);
+      hydrateOnClient();
+    } else {
+      // Workaround redux-router query string issue:
+      // https://github.com/rackt/redux-router/issues/106
+      if (routerState.location.search && !routerState.location.query) {
+        routerState.location.query = qs.parse(routerState.location.search);
+      }
+
+      store.getState().router.then(() => {
+        const component = (
+          <Provider store={store} key="provider">
+            <ReduxRouter/>
+          </Provider>
+        );
+
+        const status = getStatusFromRoutes(routerState.routes);
+        if (status) {
+          res.status(status);
+        }
+        res.send('<!doctype html>\n' +
+          ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={component} store={store}/>));
+      }).catch((err) => {
+        console.error('DATA FETCHING ERROR:', pretty.render(err));
+        res.status(500);
+        hydrateOnClient();
+      });
+    }
+  }));
+});
+
+app.listent(config.port, config.host, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 💻  Open http://localhost:%s in a browser to view the app.', config.port);
 });
